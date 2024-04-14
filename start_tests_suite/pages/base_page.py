f3527c1bccaf3d02feebc7f895ccf723d55afd3c
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2024-04-14 11:02:14 +0300

    troubleshooting in tests

--- start_tests_suite/pages/base_page.py
+++ start_tests_suite/pages/base_page.py
@@ -10,42 +10,42 @@ class BasePage:
     def check_movies_button(self):
         browser.element('[data-testid="movies_button"]').click()
         browser.element('.BreadCrumbs_breadcrumbs_point-name__6F2fE').should(
-            have.texts('Фильмы')
+            have.text("Фильмы")
         )
         return self
 
     def check_series_button(self):
         browser.element('[data-testid="series_button"]').click()
-        browser.element('Catalog_catalog__Gjv4a').should(
-            have.texts('Сериалы -  смотреть онлайн')
+        browser.element('.BreadCrumbs_breadcrumbs_point-name__6F2fE').should(
+            have.text("Сериалы")
         )
         return self
 
     def check_animation_button(self):
         browser.element('[data-testid="animation_button"]').click()
-        browser.element('.Catalog_catalog__Gjv4a').should(
-            have.texts('Мультфильмы -  смотреть онлайн')
+        browser.element('.BreadCrumbs_breadcrumbs_point-name__6F2fE').should(
+            have.text("Мультфильмы")
         )
         return self
 
     def check_new_button(self):
         browser.element('[data-testid="new_button"]').click()
-        browser.element('.Genres_new_body__wWFFU').should(
-            have.texts('Новинки фильмов, мультфильмов и сериалов смотреть онлайн ')
+        browser.element('.BreadCrumbs_breadcrumbs_point-name__6F2fE').should(
+            have.text("Новинки")
         )
         return self
 
     def check_journal_button(self):
         browser.element('[data-testid="header_journal_button"]').click()
         browser.element('.BreadCrumbs_breadcrumbs_point-name__6F2fE').should(
-            have.texts('Журнал')
+            have.text("Журнал")
         )
         return self
 
     def check_tv_button(self):
-        browser.open('[data-testid="tv_button"]').click()
-        browser.element('[data-testid="hover_play_off"]').should(
-            have.texts('ТВ Каналы Онлайн и Программа Передач')
+        browser.element('[data-testid="tv_button"]').click()
+        browser.element('.BreadCrumbs_breadcrumbs_point-name__6F2fE').should(
+            have.text("ТВ")
         )
         return self
 
