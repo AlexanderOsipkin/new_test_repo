20ed092154f7fdf81ee12bf19cec06b0e475d0cf
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2024-05-02 16:59:51 +0300

    adding allure labels

--- start_tests_suite/tests/test_login_page.py
+++ start_tests_suite/tests/test_login_page.py
@@ -2,6 +2,15 @@ import allure
 from start_tests_suite.pages.login_page import login_page
 
 
+@allure.epic('Login page')
+@allure.story('Login')
+@allure.title('Login')
+@allure.feature('PLogin page')
+@allure.label('microservice', 'login page')
+@allure.label('owner', 'Alexander Osipkin')
+@allure.tag('smoke', 'regress', 'web')
+@allure.severity('critical')
+@allure.label('layer', 'web')
 def test_login_page():
     login_page.open_maim_page()
     login_page.click_avatar_button()
