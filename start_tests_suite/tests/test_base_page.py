2583c379f52a73ff2b3c52d03bbdd258097bd212
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2024-05-16 13:13:52 +0300

    minor fix descriptions

--- start_tests_suite/tests/test_base_page.py
+++ start_tests_suite/tests/test_base_page.py
@@ -15,20 +15,20 @@ def test_base_page():
     with allure.step("Открываем главную страницу онлайн кинотеатра start.ru"):
         base_page.open_main_page()
 
-    with allure.step("Проверяем кнопку Фильмы"):
+    with allure.step("Проверяем кнопку - Фильмы"):
         base_page.check_movies_button()
 
-    with allure.step("Проверяем кнопку Сериалы"):
+    with allure.step("Проверяем кнопку - Сериалы"):
         base_page.check_series_button()
 
-    with allure.step("Проверяем кнопку Мультфильмы"):
+    with allure.step("Проверяем кнопку - Мультфильмы"):
         base_page.check_animation_button()
 
-    with allure.step("Проверяем кнопку Новинки"):
+    with allure.step("Проверяем кнопку - Новинки"):
         base_page.check_new_button()
 
-    with allure.step("Проверяем кнопку Журнал"):
+    with allure.step("Проверяем кнопку - Журнал"):
         base_page.check_journal_button()
 
-    with allure.step("Проверяем кнопку ТВ"):
+    with allure.step("Проверяем кнопку - ТВ"):
         base_page.check_tv_button()
